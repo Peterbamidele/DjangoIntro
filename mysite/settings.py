173850fc63b63ea5f4a953031ab585215797a093
Peterbamidele <peterbamidele@gmail.com>
2021-04-27 18:46:33 +0100

    add procfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--@h2ow*_y6uaj&^#9^82ig3wfp0c+(7#uohvw5#&_pog=scn$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = DEBUG = config('DEBUG', cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(config('ALLOWED_HOSTS').split(','))
